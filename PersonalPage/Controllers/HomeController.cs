89f1216d23a89cf713961bbc2d0d6bc12ec10819
Yan Li <lixx4266@umn.edu>
2019-10-07 11:28:40 -0500

    Change background image.

--- PersonalPage/Controllers/HomeController.cs
+++ PersonalPage/Controllers/HomeController.cs
@@ -33,11 +33,5 @@ namespace PersonalPage.Controllers
             ViewBag.Message = "Your application description page.";
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            return View();
-        }
     }
 }
